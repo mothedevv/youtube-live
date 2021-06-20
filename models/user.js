90ef60758d6e362200a02b85f56611dd7e4a30cc
Ryan Evans <rdevans87@gmail.com>
2021-06-20 18:25:57 -0400

    user model for authentication

--- models/user.js
+++ models/user.js
@@ -9,3 +9,46 @@ class User extends Model {
     }
   }
   
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    {
+
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            }
+          },
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'User'
+        }
+      );
+      
+      module.exports = User;
+      

d34d856ae80884f1cfd4bade311b403141b69f49
Kyle <kyleyoung.9210@gmail.com>
2021-06-23 11:36:31 -0400

    fixing imports for api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
-const router = require("express").Router();
-const postRoutes = require("./posts");
+const router = require('express').Router();
+const userRoutes = require('./user');
+const postRoutes = require('./posts');
+const videoRoutes = require('./videos');
 
 // Post routes
-router.use("/posts", postRoutes);
+router.use('/posts', postRoutes);
+router.use('/user', userRoutes);
+router.use('/video', videoRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,18 @@
-const path = require("path");
-const router = require("express").Router();
-const apiRoutes = require("./api");
-const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post');
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
 // API Routes
-router.use("/api", apiRoutes);
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
+router.use('/api', apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+router.use(function (req, res) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 module.exports = router;
 
-
 // import axios from "axios";
-
 
 // export default axios.create({
 //   baseURL: "https://www.googleapis.com/youtube/v3",

bcdd843691d1a49850ddaea51c1ac8762ac9094c
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-22 21:21:06 -0400

    working on dashboard.

---
+++ client/src/pages/dashboard/dashboard.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+
+function Profile () {
+    return (
+        <div>
+            <div class="fullwidth-block upcoming-event-section" data-bg-color="#191919">
+                    <div class="container">
+                        <header class="section-header">
+                            <h2 class="section-title">Upcoming events</h2>
+                            <div class="event-nav">
+                                <a class="prev" id="event-prev" href="#"><i class="fa fa-caret-left"></i></a>
+                                <a class="next" id="event-next" href="#"><i class="fa fa-caret-right"></i></a>
+                            </div> <!-- .event-nav -->
+                        </header> <!-- .section-header -->
+                        <div class="event-carousel">
+                            <div class="event">
+                                <div class="entry-date">
+                                    <div class="date">23</div>
+                                    <span class="month">Jun</span>
+                                </div>
+                                <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+                            </div> <!-- .event -->
+                            <div class="event">
+                                <div class="entry-date">
+                                    <div class="date">23</div>
+                                    <span class="month">Jun</span>
+                                </div>
+                                <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+                            </div> <!-- .event -->
+                            <div class="event">
+                                <div class="entry-date">
+                                    <div class="date">23</div>
+                                    <span class="month">Jun</span>
+                                </div>
+                                <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
+                            </div> <!-- .event -->
+                            <div class="event">
+                                <div class="entry-date">
+                                    <div class="date">23</div>
+                                    <span class="month">Jun</span>
+                                </div>
+                                <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
+                            </div> <!-- .event -->
+                            <div class="event">
+                                <div class="entry-date">
+                                    <div class="date">23</div>
+                                    <span class="month">Jun</span>
+                                </div>
+                                <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
+                            </div> <!-- .event -->
+                        </div> <!-- .event-carousel -->
+                    </div> <!-- .container -->
+                </div>
+        </div>
+    )
+}

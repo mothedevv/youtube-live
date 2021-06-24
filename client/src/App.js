2cd39106dc0eabe42637d12356f64f7ec020f50b
Mo Ager <moagermo@gmail.com>
2021-06-24 19:52:16 -0400

    git merges

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,6 @@
-<<<<<<< HEAD
-import React from 'react'
-import Header from './components/Header';
-import LandingPage from './components/LandingPage'
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom'
-const App = () =>
-{
-  return(
-    <Router>
-      <div>
-        <div id="site-content">
-          <Header />
-          <LandingPage />
-        </div>
-
-        
-      </div>
-    </Router>
-  )
-}
-=======
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from '../src/components/Navbar/Navbar';
+import Navbar from './components/Navbar';
 import './App.css';
 import Landing from './pages/landingPage/Landing';
 import Login from './pages/login/Login';
@@ -46,6 +23,5 @@ const App = () => {
 	);
 };
 
->>>>>>> 238121ed0798c410fd9186d453f3813689cfd7fb
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -3,19 +3,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-<<<<<<< HEAD
 
 
 ReactDOM.render(<App />, document.getElementById('root')
-=======
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
->>>>>>> 238121ed0798c410fd9186d453f3813689cfd7fb
+
 );
 

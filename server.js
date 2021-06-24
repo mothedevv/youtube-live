7db6b8114e66402da4ca46397877abc5e4bfb2c0
Kyle <kyleyoung.9210@gmail.com>
2021-06-24 11:05:51 -0400

    importing cors and using as middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +8,13 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
+
 // Add routes, both API and view
 app.use(routes);
 

39b4d794121f343cb964947c7d03e39e53c379e1
Kyle <kyleyoung.9210@gmail.com>
2021-06-24 11:53:03 -0400

    created login method using jwt and bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
 	signUp: async (req, res) => {
@@ -14,28 +15,28 @@ module.exports = {
 				!username ||
 				!password ||
 				!passwordCheck) &&
-				res.status(400).json({ msg: 'Please fill out all fields.' });
+				res.status(422).json({ msg: 'Please fill out all fields.' });
 
 			const existingEmail = await db.User.findOne({ email: email });
 			existingEmail &&
 				res
-					.status(400)
+					.status(422)
 					.json({ msg: 'The email is already registered with an account.' });
 
 			const existingUsername = await db.User.findOne({ username: username });
 			existingUsername &&
 				res
-					.status(400)
+					.status(422)
 					.json({ msg: 'That username is already taken. Please try again.' });
 
 			password.length < 6 &&
 				res
-					.status(400)
+					.status(422)
 					.json({ msg: 'Password must be at least 6 characters.' });
 
 			if (password !== passwordCheck) {
 				res
-					.status(400)
+					.status(422)
 					.json({ msg: 'Passwords do not match. Please try again.' });
 			} else {
 				const salt = await bcrypt.genSalt();
@@ -52,7 +53,47 @@ module.exports = {
 					.catch((err) => res.status(422).json(err));
 			}
 		} catch (err) {
+			res.status(422).json(err);
+		}
+	},
+
+	login: async (req, res) => {
+		try {
+			const { username, password } = req.body;
+
+			(!username || !password) &&
+				res.status(422).json({ msg: 'Please fill out all fields.' });
+
+			const user = await db.User.findOne({ username: username });
+
+			!user &&
+				res
+					.status(422)
+					.json({ msg: 'Username has not been registered. Please try again.' });
+
+			const isMatch = await bcrypt.compare(password, user.password);
+			!isMatch &&
+				res.status(422).json({ msg: 'Incorrect password. Please try again.' });
+
+			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+			res.json({
+				token,
+				user: {
+					id: user._id,
+					firstName: user.firstName,
+					lastName: user.lastName,
+					email: user.email,
+					username: username,
+				},
+			});
+		} catch (err) {
 			res.status(422).json(err.message);
 		}
 	},
+
+	// logout: (req, res) => {
+	// 	res.cookie('jwt', '', { maxAge: 1 });
+	// 	res.redirect('/');
+	// },
 };

4f859535d687acac8320bf9d279dc13cff9e4736
Kyle <kyleyoung.9210@gmail.com>
2021-06-24 09:37:36 -0400

    previously committed model was coded for sequelize use. fixed for mongoose using mongoose and bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,54 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const sequelize = require('../config/config');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-class User extends Model {
+const UserSchema = new mongoose.Schema({
+	username: String,
+	password: String,
+});
 
-    checkPassword(loginPw) {
-      return bcrypt.compareSync(loginPw, this.password);
-    }
-  }
-  
-User.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4]
-        }
-      }
-    },
-    {
+// Mongoose pre middleware to salt and hash passwords before being saved to database
+UserSchema.pre('save', function (next) {
+	const user = this;
 
-        hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
-            beforeCreate: async (newUserData) => {
-              newUserData.password = await bcrypt.hash(newUserData.password, 10);
-              return newUserData;
-            },
-            beforeUpdate: async (updatedUserData) => {
-              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-              return updatedUserData;
-            }
-          },
-          sequelize,
-          timestamps: false,
-          freezeTableName: true,
-          underscored: true,
-          modelName: 'User'
-        }
-      );
-      
-      module.exports = User;
-      
+	// if password has been modified (changed by user) OR a new user and password has been entered,
+	if (this.isModified('password') || this.isNew) {
+		// then hash password
+		bcrypt.genSalt(10, function (saltError, salt) {
+			if (saltError) {
+				return next(saltError);
+			} else {
+				bcrypt.hash(user.password, salt, function (hashError, hash) {
+					if (hashError) {
+						return next(hashError);
+					}
+
+					user.password = hash;
+					next();
+				});
+			}
+		});
+	} else {
+		// otherwise, end middleware and move on
+		return next();
+	}
+});
+
+module.exports = mongoose.model('User', UserSchema);

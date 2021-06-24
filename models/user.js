99ce223ab021e5d70709b9bde7ccb28ed991a6d1
Kyle <kyleyoung.9210@gmail.com>
2021-06-24 11:08:52 -0400

    making UserSchema a little more simple. removing bcrypt and using it in the user controller instead

--- models/user.js
+++ models/user.js
@@ -1,36 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-	username: String,
-	password: String,
-});
-
-// Mongoose pre middleware to salt and hash passwords before being saved to database
-UserSchema.pre('save', function (next) {
-	const user = this;
-
-	// if password has been modified (changed by user) OR a new user and password has been entered,
-	if (this.isModified('password') || this.isNew) {
-		// then hash password
-		bcrypt.genSalt(10, function (saltError, salt) {
-			if (saltError) {
-				return next(saltError);
-			} else {
-				bcrypt.hash(user.password, salt, function (hashError, hash) {
-					if (hashError) {
-						return next(hashError);
-					}
-
-					user.password = hash;
-					next();
-				});
-			}
-		});
-	} else {
-		// otherwise, end middleware and move on
-		return next();
-	}
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	password: { type: String, required: true, minlength: 6 },
 });
 
 module.exports = mongoose.model('User', UserSchema);

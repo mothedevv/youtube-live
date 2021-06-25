c1f478839b9a1e5ebec2e391c1bddc31339fdcd5
Kyle <kyleyoung.9210@gmail.com>
2021-06-25 17:11:05 -0400

    removed intial value of false from loggedIn state

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const UserContext = createContext();
 
 export const UserProvider = (props) => {
-	const [loggedIn, setLoggedIn] = useState(false);
+	const [loggedIn, setLoggedIn] = useState();
 	return (
 		<UserContext.Provider value={[loggedIn, setLoggedIn]}>
 			{props.children}

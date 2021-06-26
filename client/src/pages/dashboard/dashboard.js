34677a4cb858f634c8a44506bc40984eeb487199
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-26 10:14:42 -0400

    cleaned up dashbaord.

--- client/src/pages/dashboard/dashboard.js
+++ client/src/pages/dashboard/dashboard.js
@@ -11,8 +11,8 @@ function Profile () {
                             <div class="event-nav">
                                 <a class="prev" id="event-prev" href="#"><i class="fa fa-caret-left"></i></a>
                                 <a class="next" id="event-next" href="#"><i class="fa fa-caret-right"></i></a>
-                            </div> <!-- .event-nav -->
-                        </header> <!-- .section-header -->
+                            </div> 
+                        </header> 
                         <div class="event-carousel">
                             <div class="event">
                                 <div class="entry-date">
@@ -21,7 +21,7 @@ function Profile () {
                                 </div>
                                 <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
-                            </div> <!-- .event -->
+                            </div> 
                             <div class="event">
                                 <div class="entry-date">
                                     <div class="date">23</div>
@@ -29,7 +29,7 @@ function Profile () {
                                 </div>
                                 <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
-                            </div> <!-- .event -->
+                            </div> 
                             <div class="event">
                                 <div class="entry-date">
                                     <div class="date">23</div>
@@ -37,7 +37,7 @@ function Profile () {
                                 </div>
                                 <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur</p>
-                            </div> <!-- .event -->
+                            </div> 
                             <div class="event">
                                 <div class="entry-date">
                                     <div class="date">23</div>
@@ -45,7 +45,7 @@ function Profile () {
                                 </div>
                                 <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
-                            </div> <!-- .event -->
+                            </div> 
                             <div class="event">
                                 <div class="entry-date">
                                     <div class="date">23</div>
@@ -53,9 +53,9 @@ function Profile () {
                                 </div>
                                 <h2 class="entry-title"><a href="#">Sed et persipiatis unde omnis iste natus</a></h2>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur </p>
-                            </div> <!-- .event -->
-                        </div> <!-- .event-carousel -->
-                    </div> <!-- .container -->
+                            </div> 
+                        </div> 
+                    </div> 
                 </div>
         </div>
     )

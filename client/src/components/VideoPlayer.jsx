eadde4b9659f0fa85c5363328569739c1aae46e4
Ryan Evans <rdevans87@gmail.com>
2021-06-28 07:24:44 -0400

    creates pages folder and file for video area for video components

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,67 +1,27 @@
-// import React from "react";
+import React from "react";
 
 
-// function VideoPlayer() 
+const VideoPlayer = ({ video }) => {
+  if (!video) {
+    return <div>Loading Video Player...</div>;
+  }
+const videoId = video.id.videoId;
+const url = `https://youtube.com/embed/${videoId}`;
 
-// {
+return (
+  <div className="video-detail col-md-8" id="player">
+ <div className="embed-responsive embed responsive=16by9">
+   <iframe className="embed-responsive-item" src={url}></iframe>
+ </div>
+ <div className="details">
+   <div>{video.snippet.title}</div>
+   <div>{video.snippet.description}</div>
+ </div>
+  </div>
 
-  // tag.id = 'iframe-demo';
-  // tag.src = 'https://www.youtube.com/iframe_api';
-  // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+);
 
-  // var player;
-  // function onYouTubeIframeAPIReady() {
-  // player = new player('existing-iframe-example', {
-  //       events: {
-  //         'onReady': onPlayerReady,
-  //         'onStateChange': onPlayerStateChange
-  //       }
-  //   });
-  // }
+}
 
-
-
-  // function onPlayerReady(event) {
-  //   document.getElementById('existing-iframe-example').style.borderColor = '#FF6D00';
-  // }
-  // function changeBorderColor(playerStatus) {
-  //  var color;
-  //   if (playerStatus == -1) {
-  //     color = "#37474F"; // unstarted = gray
-  //   } else if (playerStatus == 0) {
-  //     color = "#FFFF00"; // ended = yellow
-  //   } else if (playerStatus == 1) {
-  //     color = "#33691E"; // playing = green
-  //   } else if (playerStatus == 2) {
-  //     color = "#DD2C00"; // paused = red
-  //   } else if (playerStatus == 3) {
-  //     color = "#AA00FF"; // buffering = purple
-  //   } else if (playerStatus == 5) {
-  //     color = "#FF6DOO"; // video cued = orange
-  //   }
-  //   if (color) {
-  //     document.getElementById('existing-iframe-example').style.borderColor = color;
-  //   }
-  // }
-  // function onPlayerStateChange(event) {
-  //   changeBorderColor(event.data);
-  // }
-
-
-  
-
-
-//   return(
-//     <iframe id="existing-iframe-example"
-//      width="640" height="360"
-//      src="https://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1"
-//      frameborder="0"
-//      style="border: solid 4px #37474F"
-//     ></iframe>
-//   );
-  
-// };
-
-
-// export default VideoPlayer;
+export default VideoPlayer;
 

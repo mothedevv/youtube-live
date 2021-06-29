4e72ed0f1cebd2af9da4ff4e91ceb1a66a20cbd9
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-29 17:45:35 -0400

    changed colors of nav

---
+++ client/src/components/Profilenav.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+
+function Profilenavbar(){
+	
+
+	return (
+		<nav class="navbar">
+						<div class="container">
+							<div class="navbar-brand">
+								<span class="navbar-burger burger" data-target="navbarMenu">
+									<span></span>
+									<span></span>
+									<span></span>
+								</span>
+							</div>
+							<div id="navbarMenu" class="navbar-menu">
+								<div class="navbar-end">
+									<span class="navbar-item">
+										<a class="button is-white is-outlined">
+											<span class="icon">
+												<i class="fa fa-home"></i>
+											</span>
+											<span>Home</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-white is-outlined">
+											<span class="icon">
+												<i class="fas fa-guitar"></i>
+											</span>
+											<span>Venues</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-white is-outlined">
+											<span class="icon">
+												<i class="fas fa-music"></i>
+											</span>
+											<span>Artists</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-white is-outlined" >
+											<span class="icon">
+												<i class="fas fa-user-friends"></i>
+											</span>
+											<span>Social</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-white is-outlined" >
+											<span class="icon">
+												<i class="fas fa-question"></i>
+											</span>
+											<span>Try it out</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-purple is-outlined" >
+											<span class="icon">
+												<i class="fas fa-sign-in-alt"></i>
+											</span>
+											<span>Login</span>
+										</a>
+									</span>
+									<span class="navbar-item">
+										<a class="button is-purple is-outlined" >
+											<span class="icon">
+												<i class="fas fa-user-plus"></i>
+											</span>
+											<span>Signup</span>
+										</a>
+									</span>
+								</div>
+							</div>
+						</div>
+					</nav>
+	);
+};
+
+export default Profilenavbar;

6ffd9c8b2effe7356cc3389ce04e85f33bbb43c5
Kyle <kyleyoung.9210@gmail.com>
2021-07-01 18:38:19 -0400

    fixing merge conflict

--- client/src/pages/venue/venue.js
+++ client/src/pages/venue/venue.js
@@ -1,28 +1,11 @@
-import React from "react";
-<<<<<<< HEAD
-import Venue from './nationwide.jpeg'
-
-function Venues(){
-    return (
-        <div>
-          <div class="container">
-              
-          </div>
-            
-        </div>    
-=======
-import Navbar from "../../components/Navbar";
-import Card from "../../components/card";
-import Footer from "../../components/Footer";
-function Venue(){
-    return (
-        <div>
-            <Navbar />
-            <Card />
-            <Footer />
-        </div>
->>>>>>> 2b2d08316092a4039da1c412f10afae21f4a33ca
-    )
+import React from 'react';
+import Card from '../../components/card';
+function Venues() {
+	return (
+		<div>
+			<Card />
+		</div>
+	);
 }
 
 export default Venues;

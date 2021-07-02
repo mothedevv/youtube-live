0adee2b566fc79538b60da54ccc22989308effff
Kyle <kyleyoung.9210@gmail.com>
2021-07-02 11:37:07 -0400

    changed padding around footer to make it a little smaller

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
 import 'bulma/css/bulma.min.css';
+import React from 'react';
 
-const Footer = () =>
-{
-    return(
-        <footer className="footer">
-            <div className="container has-text-centered">
-                <p className="copy">Copyright 2021 YouTube.Live, Designed by Mo Ager, Anthony DeCapite, Morgan Schall, Kyle Young and Ryan Evans. All rights reserved</p>
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+	return (
+		<footer className="footer" style={{ padding: '1.5em' }}>
+			<div className="container has-text-centered">
+				<p className="copy">
+					Copyright 2021 YouTube.Live, Designed by Mo Ager, Anthony DeCapite,
+					Morgan Schall, Kyle Young and Ryan Evans. All rights reserved
+				</p>
+			</div>
+		</footer>
+	);
+};
 
 export default Footer;

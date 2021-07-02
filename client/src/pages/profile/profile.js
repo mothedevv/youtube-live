74e71aac9bd4ad589aea10bab12d508e752cbdc8
Anthony DeCapite <anthony.decapite@icloud.com>
2021-07-01 21:01:23 -0400

    building out profile page.

---
+++ client/src/pages/profile/profile.js
@@ -0,0 +1,16 @@
+import React from "react";
+import 'bulma/css/bulma.min.css';
+
+function Profile() {
+    return(
+        <div>
+            <button class="button is-danger is-outlined">Button</button>
+            <div class="card">
+            <div class="card-image">
+                <img>
+                </img>
+            </div>
+            </div>
+        </div>
+    )
+}

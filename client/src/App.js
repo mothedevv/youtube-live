0924afd5f2866c3845afe1443c64edf3438e0f34
Kyle <kyleyoung.9210@gmail.com>
2021-07-06 20:23:39 -0400

    put updated navbar back in after being removed on accident

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { UserProvider } from './utils/UserContext';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import Navbar from './components/Nav/Navbar';
+import LandingPage from './pages/landingPage/Landing';
+import Login from './pages/login/Login';
+import MyArtists from './pages/MyArtists/MyArtists';
+import MyVenues from './pages/MyVenues/MyVenues';
+import Profile from './pages/Profile/Profile.jsx';
+import Search from './pages/Search/Search';
+import SignUp from './pages/signup/SignUp';
+import Venues from './pages/venue/venue';
+import { UserProvider } from './utils/UserContext';
 
 const App = () => {
 	return (
@@ -11,9 +19,17 @@ const App = () => {
 				<div>
 					<Navbar />
 					<div className="container">
-						
+						<Switch>
+							<Route exact path="/" component={LandingPage} />
+							<Route exact path="/signup" component={SignUp} />
+							<Route exact path="/login" component={Login} />
+							<Route exact path="/venues" component={Venues} />
+							<Route exact path="/profile" component={Profile} />
+							<Route exact path="/myartists" component={MyArtists} />
+							<Route exact path="/myvenues" component={MyVenues} />
+							<Route exact path="/search" component={Search} />
+						</Switch>
 					</div>
-
 				</div>
 				<Footer />
 			</UserProvider>

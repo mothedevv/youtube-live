16aaf1d982135c32c4593b914287ed070bbea497
Morgan Schall <morgan.allison.schall@gmail.com>
2021-07-07 21:06:24 -0400

    deleting video routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
+//const postRoutes = require('./posts');
 
+// Post routes
+//router.use('/posts', postRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;

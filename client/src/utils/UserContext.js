f2981a21c2ae883f2fc6778b570641acd344b6d8
Kyle <kyleyoung.9210@gmail.com>
2021-07-08 15:22:10 -0400

    simplified UserContext functionality using useMemo()

--- client/src/utils/UserContext.js
+++ client/src/utils/UserContext.js
@@ -1,12 +1,3 @@
-import React, { createContext, useState } from 'react';
+import { createContext } from 'react';
 
-export const UserContext = createContext();
-
-export const UserProvider = (props) => {
-	const [loggedIn, setLoggedIn] = useState();
-	return (
-		<UserContext.Provider value={[loggedIn, setLoggedIn]}>
-			{props.children}
-		</UserContext.Provider>
-	);
-};
+export const UserContext = createContext(null);

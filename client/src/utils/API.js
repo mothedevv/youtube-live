3d05b994ab2b78902027589b70112365bb1b2848
Morgan Schall <morgan.allison.schall@gmail.com>
2021-07-07 21:13:46 -0400

    cleanup

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -15,16 +15,3 @@ export default axios.create({
 });
 
 
-
-//SeatGeek ClientID
-const CLIENT_ID = process.env.REACT_APP_SEATGEEK_CLIENT_ID
-const url = 'https://api.seatgeek.com/2/venues?city=';
-export default {
-	getVenues: (query) => {
-		console.log(url + query + `&client_id=${CLIENT_ID}`);
-		return axios.get(url + query + `&client_id=${CLIENT_ID}`);
-	},
-	getPerformers: (query) => {
-		return axios.get(url + query + `&client_id=${CLIENT_ID}`);
-	},
-};

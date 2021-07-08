55f06e471478b8c278e95e1f63ab88dbd3fea9a5
Morgan Schall <morgan.allison.schall@gmail.com>
2021-07-07 21:11:07 -0400

    Revert "deleting video routes"
    This reverts commit 16aaf1d982135c32c4593b914287ed070bbea497.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
-//const postRoutes = require('./posts');
 
-// Post routes
-//router.use('/posts', postRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;

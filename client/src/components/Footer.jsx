0510676ba179a988347ec5990a8b68d25d1666f7
Kyle <kyleyoung.9210@gmail.com>
2021-07-08 13:33:16 -0400

    changing layout of footer.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import 'bulma/css/bulma.min.css';
 
-const Footer = () =>
-{
-    return(
-        <footer className="footer">
-            <div className="container has-text-centered">
-                <p className="copy">Copyright 2021 YouTube.Live, Designed by Mo Ager, Anthony DeCapite, Morgan Schall, Kyle Young and Ryan Evans. All rights reserved</p>
-            </div>
-        </footer>
-    )
-}
+const Footer = () => {
+	return (
+		<footer className="footer p-2">
+			<div className="container has-text-centered">
+				<p className="copy p-1">
+					Designed by Mo Ager, Anthony DeCapite, Morgan Schall, Kyle Young and
+					Ryan Evans.
+				</p>
+				<p className="copy p-1">All rights reserved</p>
+				<p className="copy p-1">Copyright 2021 YouTube.Live</p>
+			</div>
+		</footer>
+	);
+};
 
 export default Footer;

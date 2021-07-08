c9cc356b0e3784ebeebbb81597ce52fc3a760373
Morgan Schall <morgan.allison.schall@gmail.com>
2021-07-07 21:03:03 -0400

    removed video routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 //Youtube API key
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
+
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
@@ -14,3 +15,16 @@ export default axios.create({
 });
 
 
+
+//SeatGeek ClientID
+const CLIENT_ID = process.env.REACT_APP_SEATGEEK_CLIENT_ID
+const url = 'https://api.seatgeek.com/2/venues?city=';
+export default {
+	getVenues: (query) => {
+		console.log(url + query + `&client_id=${CLIENT_ID}`);
+		return axios.get(url + query + `&client_id=${CLIENT_ID}`);
+	},
+	getPerformers: (query) => {
+		return axios.get(url + query + `&client_id=${CLIENT_ID}`);
+	},
+};
